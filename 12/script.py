d52ca9cf67d3a10d213496d634a85d738046b558
Sjoerd Nijhof <sjoerd.vdbos@hotmail.com>
2020-12-19 13:38:21 +0100

    Day 12

--- 12/script.py
+++ 12/script.py
@@ -3,7 +3,7 @@ f = open("input.txt", "r")
 inp = f.readlines()
 f.close()
 
-# part 1 2
+# part 1
 import math
 
 class Boat:
@@ -43,14 +43,88 @@ class Boat:
             self.rl(val)
         elif cmd == 'F':
             self.f(val)
+            
+# part 2
+class BoatWithWayPoint:
+    def __init__(self):
+        # waypoint relative to ship pos
+        self.wpx = 10
+        self.wpy = 1
+        # ship pos
+        self.posx = 0
+        self.posy = 0
+    
+    def doCmd(self,cmd,val):
+        if cmd == 'N':
+            self.n(val)
+        elif cmd == 'S':
+            self.s(val)
+        elif cmd == 'E':
+            self.e(val)
+        elif cmd == 'W':
+            self.w(val)
+        elif cmd == 'R':
+            self.rr(val)
+        elif cmd == 'L':
+            self.rl(val)
+        elif cmd == 'F':
+            self.f(val)
 
-b = Boat()
+    def e(self,d):
+        self.wpx = self.wpx +d
+    def w(self,d):
+        self.wpx = self.wpx -d
+    def n(self,d):
+        self.wpy = self.wpy +d
+    def s(self,d):
+        self.wpy = self.wpy -d
+    def rl(self,anAng):
+        myXold = self.wpx
+        self.wpx = myXold * round(math.cos(anAng/180*math.pi)) - self.wpy * round(math.sin(anAng/180*math.pi))
+        self.wpy = myXold * round(math.sin(anAng/180*math.pi)) + self.wpy * round(math.cos(anAng/180*math.pi))
+    def rr(self,anAng):
+        myXold = self.wpx
+        self.wpx = myXold * round(math.cos(anAng/180*math.pi)) + self.wpy * round(math.sin(anAng/180*math.pi))
+        self.wpy =-myXold * round(math.sin(anAng/180*math.pi)) + self.wpy * round(math.cos(anAng/180*math.pi))
+    def f(self,d):
+        self.posx = self.posx + d * self.wpx
+        self.posy = self.posy + d * self.wpy
+
+# b = Boat()
+b = BoatWithWayPoint()
 for i in range(len(inp)):
     cmd = inp[i][0]
     val = int(inp[i][1:-1])
     # print(inp[i],cmd,val)
     b.doCmd(cmd,val)
     # print(b.posx,b.posy,b.ang)
+    # print(b.posx,b.posy,b.wpx,b.wpy)
 
 print(b.posx,b.posy)
 print(abs(b.posx)+abs(b.posy))
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
